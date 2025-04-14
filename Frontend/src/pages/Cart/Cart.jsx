c30503364858483b0ead0571fc6d8039514a1d46
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2025-04-14 12:00:12 +0530

    add frontend

---
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from 'react'
+import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
+
+const Cart = () => {
+
+  const {cartItems,food_list,removeFromCart,getToalCartAmount} = useContext(StoreContext);
+
+  return (
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index)=>{
+          /*ppppppppppppppppppppppppppp*/
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getToalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getToalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='promo code'/>
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+  }
+
+  export default Cart
